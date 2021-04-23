ea03a934c4d7b33f70334ce9e40b3e217f65e5d3
Sean Mortimer <sean.mortimer@gmail.com>
2021-04-23 17:27:48 -0600

    Update user api

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+        fetch('https://jsonplaceholder.cypress.io/users').then(response => {
             return response.json();
         }).then(users => {
             this.setState({ robots: users })
